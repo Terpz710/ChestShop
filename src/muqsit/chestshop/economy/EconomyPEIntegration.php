48eccc8cf54ab65b487f0b636aec6f452b902266
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-12-29 12:34:35 -0800

    Update and rename EconomyAPIIntegration.php to EconomyPEIntegration.php

---
+++ src/muqsit/chestshop/economy/EconomyPEIntegration.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace muqsit\chestshop\economy;
+
+use Closure;
+use muqsit\chestshop\util\PlayerIdentity;
+use Terpz710\EconomyPE\Money;
+
+final class EconomyPEIntegration implements EconomyIntegration{
+
+	public function init(array $config) : void
+	}
+
+	public function getMoney(PlayerIdentity $player, Closure $callback) : void{
+		Money::getInstance()->getServer()->getScheduler()->scheduleDelayedTask(new EconomyPETask($player, $callback), 1);
+	}
+
+	public function addMoney(PlayerIdentity $player, float $money) : void{
+		Money::addMoney($player->getGamertag(), (int) $money);
+	}
+
+	public function removeMoney(PlayerIdentity $player, float $money, Closure $callback) : void{
+		Money::removeMoney($player->getGamertag(), (int) $money);
+		$callback(true);
+	}
+
+	public function formatMoney(float $money) : string{
+		return Money::getConfigReplace("money_format", ["{money}"], [number_format($money)]);
+	}
+}
